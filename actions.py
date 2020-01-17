067aefff08cbb7d3e2b6c8c27efa3a3dc85e3699
shammy45 <38557157+shammy45@users.noreply.github.com>
2020-01-17 14:45:29 +0530

    Add files via upload

---
+++ actions.py
@@ -0,0 +1,31 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+import requests
+
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+
+class ActionWeather(Action):
+    def name(self):
+        return 'action_weather'
+
+    def run(self, dispatcher, tracker, domain):
+        loc = tracker.get_slot('location')
+        params = {
+          'access_key': '731d26f00dee42794c56da7769d0af35',
+          'query': loc
+        }
+        api_result = requests.get('http://api.weatherstack.com/current', params)
+        api_response = api_result.json()
+        response = 'Current temperature in %s is %d℃' % (api_response['location']['name'], api_response['current']['temperature'])
+        #response = "weather is absolutely fantastic in %s" %loc
+        dispatcher.utter_message(response)
+        return [SlotSet('location',loc)]
